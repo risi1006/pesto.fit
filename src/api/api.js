8e0fa74c0f05cdae4bae2c4359f88e0b96a870bb
Ritesh Singh <risi1006@gmail.com>
2023-10-10 21:41:22 +0530

    removed api key

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ export const fetchWorkoutPlan = async(body) => {
     let data = await axios.post('https://api.openai.com/v1/chat/completions', body, {
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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',
+        'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`,
         'model': 'gpt-3.5-turbo'
     }
     }).then(({data}) => {return JSON.parse(data?.choices[0]?.message?.content)});
